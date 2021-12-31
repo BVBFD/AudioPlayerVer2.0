247828c4c03f24e20dc98ab24c9f11a54ab4611a
BVBFD <lsevina126@gmail.com>
2021-12-31 21:34:11 +0900

    Code JavaScript For Mp3 Player

--- script.js
+++ script.js
@@ -1,28 +1,22 @@
-let previous = document.querySelector("#pre");
-let play = document.querySelector("#play");
-let next = document.querySelector("#next");
-let title = document.querySelector("#title");
-let recent_volume = document.querySelector("#volume");
+let track = document.createElement("audio");
+let play = document.querySelector("#playSong");
+let musicTitle = document.querySelector("#musicTitle");
+let musicSinger = document.querySelector("#musicSinger");
+let musicImg = document.querySelector("#musicImg");
+let durationSlider = document.querySelector("#durationSlider");
+let volume = document.querySelector("#volume");
 let volume_show = document.querySelector("#volume_show");
-let slider = document.querySelector("#duration_slider");
-let show_duration = document.querySelector("#show_duration");
-let track_image = document.querySelector("#track_image");
-let auto_play = document.querySelector("#auto");
 let present = document.querySelector("#present");
 let total = document.querySelector("#total");
-let artist = document.querySelector("#artist");
+let autoPlayBtn = document.querySelector("#autoPlay");
 
+let playingSong = false;
+let indexNum = 0;
+let autoPlay = false;
 let timer;
-let autoplay = 0;
+let volumeTrueFalse = true;
 
-let index_no = 0;
-let Playing_song = false;
-
-//create a audio Element
-let track = document.createElement("audio");
-
-//All songs list
-let All_song = [
+let allSong = [
   {
     name: "Plastic Love.mp3",
     path: "music/타케우치 마리야 - Plastic Love.mp3",
@@ -31,8 +25,8 @@ let All_song = [
   },
   {
     name: "second song",
-    path: "music/song2.mp3",
-    img: "img/img2.jpg",
+    path: "music/타케우치 마리야 - Plastic Love.mp3",
+    img: "img/bg.png",
     singer: "2",
   },
   {
@@ -55,126 +49,125 @@ let All_song = [
   },
 ];
 
-// All functions
+const autoPlaySong = () => {
+  if (autoPlay === false) {
+    autoPlay = true;
+    autoPlayBtn.style.background = "#ff8a65";
+  } else {
+    autoPlay = false;
+    autoPlayBtn.style.background = "#798591";
+  }
+};
 
-// function load the track
-function load_track(index_no) {
-  clearInterval(timer);
-  reset_slider();
+const volumeOnOffBtn = () => {
+  if (volumeTrueFalse === true) {
+    volumeTrueFalse = false;
+    muteSound();
+  } else {
+    volumeTrueFalse = true;
+    resetSound();
+  }
+};
 
-  track.src = All_song[index_no].path;
-  title.innerHTML = All_song[index_no].name;
-  track_image.src = All_song[index_no].img;
-  artist.innerHTML = All_song[index_no].singer;
-  track.load();
-
-  timer = setInterval(range_slider, 1000);
-  total.innerHTML = All_song.length;
-  present.innerHTML = index_no + 1;
-}
-
-load_track(index_no);
-
-//mute sound function
-function mute_sound() {
+const muteSound = () => {
   track.volume = 0;
   volume.value = 0;
   volume_show.innerHTML = 0;
-}
+};
 
-// checking.. the song is playing or not
-function justplay() {
-  if (Playing_song == false) {
-    playsong();
-  } else {
-    pausesong();
-  }
-}
+const resetSound = () => {
+  track.volume = 0.9;
+  volume.value = 90;
+  volume_show.innerHTML = 90;
+};
 
-// reset song slider
-function reset_slider() {
-  slider.value = 0;
-}
+const loadTrack = (indexNum) => {
+  track.src = allSong[indexNum].path;
+  musicImg.src = allSong[indexNum].img;
+  musicTitle.innerHTML = allSong[indexNum].name;
+  musicSinger.innerHTML = allSong[indexNum].singer;
 
-// play song
-function playsong() {
+  total.innerHTML = allSong.length;
+  present.innerHTML = indexNum + 1;
+};
+
+loadTrack(indexNum);
+
+const playSong = () => {
+  playingSong = true;
+  play.innerHTML = '<i class="fa fa-pause"></i>';
+  timer = setInterval(range_slider, 1000);
   track.play();
-  Playing_song = true;
-  play.innerHTML = '<i class="fa fa-pause" aria-hidden="true"></i>';
-}
+};
 
-//pause song
-function pausesong() {
+const stopSong = () => {
+  playingSong = false;
+  clearInterval(timer);
+  play.innerHTML = '<i class="fa fa-play"></i>';
   track.pause();
-  Playing_song = false;
-  play.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>';
-}
+};
 
-// next song
-function next_song() {
-  if (index_no < All_song.length - 1) {
-    index_no += 1;
-    load_track(index_no);
-    playsong();
+const justPlay = () => {
+  if (playingSong === false) {
+    playSong();
   } else {
-    index_no = 0;
-    load_track(index_no);
-    playsong();
+    stopSong();
   }
-}
+};
 
-// previous song
-function previous_song() {
-  if (index_no > 0) {
-    index_no -= 1;
-    load_track(index_no);
-    playsong();
+const previousSong = () => {
+  if (indexNum > 0) {
+    indexNum -= 1;
+    loadTrack(indexNum);
+    playSong();
   } else {
-    index_no = All_song.length;
-    load_track(index_no);
-    playsong();
+    indexNum = allSong.length;
+    loadTrack(indexNum);
+    playSong();
   }
-}
+};
 
-// change volume
-function volume_change() {
-  volume_show.innerHTML = recent_volume.value;
-  track.volume = recent_volume.value / 100;
-}
-
-// change slider position
-function change_duration() {
-  slider_position = track.duration * (slider.value / 100);
-  track.currentTime = slider_position;
-}
-
-// autoplay function
-function autoplay_switch() {
-  if (autoplay == 1) {
-    autoplay = 0;
-    auto_play.style.background = "rgba(255,255,255,0.2)";
+const nextSong = () => {
+  if (indexNum < allSong.length - 1) {
+    indexNum += 1;
+    loadTrack(indexNum);
+    playSong();
   } else {
-    autoplay = 1;
-    auto_play.style.background = "#FF8A65";
+    indexNum = 0;
+    loadTrack(indexNum);
+    playSong();
   }
-}
+};
 
-function range_slider() {
+const changeDuration = () => {
+  durationSliderPosition = track.duration * (durationSlider.value / 100);
+  track.currentTime = durationSliderPosition;
+};
+
+const changeVolume = () => {
+  volume_show.innerHTML = volume.value;
+  track.volume = volume.value / 100;
+};
+
+const range_slider = () => {
   let position = 0;
-
   // update slider position
   if (!isNaN(track.duration)) {
+    console.log(track.currentTime);
+    console.log(track.duration);
     position = track.currentTime * (100 / track.duration);
-    slider.value = position;
+    durationSlider.value = position;
   }
 
   // function will run when the song is over
   if (track.ended) {
-    play.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>';
-    if (autoplay == 1) {
-      index_no += 1;
-      load_track(index_no);
-      playsong();
+    play.innerHTML = '<i class="fa fa-play"></i>';
+    if (autoPlay === true) {
+      indexNum += 1;
+      loadTrack(indexNum);
+      playSong();
+    } else {
+      stopSong();
     }
   }
-}
+};
